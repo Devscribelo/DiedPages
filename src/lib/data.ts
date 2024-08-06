37aa1de583032e671c4ac0d7cb5ada839e3eb1f9
MBAPEINT <95538216+MBAPEINT@users.noreply.github.com>
2024-08-06 13:56:34 -0500

    json

---
+++ src/lib/data.ts
@@ -0,0 +1,177 @@
+export interface ImagesBiography {
+    name: string;
+    src: string;
+}
+
+export const bimages: ImagesBiography[] = [
+    {
+        name: 'biography1',
+        src: '/biography/1.jpeg'
+    },
+    {
+        name: 'biography2',
+        src: '/biography/2.jpeg'
+    },
+    {
+        name: 'biography3',
+        src: '/biography/3.jpeg'
+    },
+    {
+        name: 'biography4',
+        src: '/biography/4.jpeg'
+    },
+    {
+        name: 'biography5',
+        src: '/biography/5.jpeg'
+    },
+    {
+        name: 'biography6',
+        src: '/biography/6.jpeg'
+    },
+    {
+        name: 'biography7',
+        src: '/biography/7.jpeg'
+    },
+    {
+        name: 'biography8',
+        src: '/biography/8.jpeg'
+    }
+]
+
+export interface ImagesGallery {
+    name: string;
+    src: string;
+}
+
+export const gimages: ImagesGallery[] = [
+    {
+        name: 'gallery1',
+        src: '/gallery/1.jpg'
+    },
+    {
+        name: 'gallery2',
+        src: '/gallery/2.jpg'
+    },
+    {
+        name: 'gallery3',
+        src: '/gallery/3.jpg'
+    },
+    {
+        name: 'gallery4',
+        src: '/gallery/4.jpg'
+    },
+    {
+        name: 'gallery5',
+        src: '/gallery/5.jpg'
+    },
+    {
+        name: 'gallery6',
+        src: '/gallery/6.jpg'
+    },
+    {
+        name: 'gallery7',
+        src: '/gallery/7.jpg'
+    },
+    {
+        name: 'gallery8',
+        src: '/gallery/8.jpg'
+    },
+    {
+        name: 'gallery9',
+        src: '/gallery/9.jpg'
+    },
+    {
+        name: 'gallery10',
+        src: '/gallery/10.jpg'
+    },
+    {
+        name: 'gallery11',
+        src: '/gallery/11.jpg'
+    },
+    {
+        name: 'gallery12',
+        src: '/gallery/12.jpg'
+    },
+    {
+        name: 'gallery13',
+        src: '/gallery/13.jpg'
+    },
+    {
+        name: 'gallery14',
+        src: '/gallery/14.jpg'
+    },
+    {
+        name: 'gallery15',
+        src: '/gallery/15.jpg'
+    },
+    {
+        name: 'gallery16',
+        src: '/gallery/16.jpg'
+    },
+    {
+        name: 'gallery17',
+        src: '/gallery/17.jpg'
+    },
+    {
+        name: 'gallery18',
+        src: '/gallery/18.jpg'
+    },
+    {
+        name: 'gallery19',
+        src: '/gallery/19.jpg'
+    },
+    {
+        name: 'gallery20',
+        src: '/gallery/20.jpg'
+    }
+]
+
+export interface BVideos{
+    src: string;
+}
+
+export const bvideos: BVideos[] = [
+    {
+        src:'https://www.youtube.com/embed/dQw4w9WgXcQ'
+    },
+    {
+        src:'https://www.youtube.com/embed/3JZ_D3ELwOQ'
+    }
+]
+
+export interface GVideos{
+    src: string;
+}
+
+export const gvideos: GVideos[] = [
+    {
+        src:'https://www.youtube.com/embed/dQw4w9WgXcQ'
+    },
+    {
+        src:'https://www.youtube.com/embed/3JZ_D3ELwOQ'
+    },
+    {
+        src:'https://www.youtube.com/embed/ZXsQAXx_ao0'
+    },
+    {
+        src:'https://www.youtube.com/embed/lY2H2ZP56K4'
+    },
+    {
+        src:'https://www.youtube.com/embed/f15hLBZpMv8'
+    },
+    {
+        src:'https://www.youtube.com/embed/dQw4w9WgXcQ'
+    },
+    {
+        src:'https://www.youtube.com/embed/aKX62HrwdJ8'
+    },
+    {
+        src:'https://www.youtube.com/embed/J5R7wxM59rk'
+    },
+    {
+        src:'https://www.youtube.com/embed/1dc0g7Ne2Bk'
+    },
+    {
+        src:'https://www.youtube.com/embed/ETh1wBN5Oq8'
+    }
+]
