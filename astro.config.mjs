da8b0a9145d080a2dbca1b166576d0dfb3afce1b
MBAPEINT <95538216+MBAPEINT@users.noreply.github.com>
2024-08-06 19:23:13 -0500

    netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   devToolbar: {
     enabled: false
   },
   integrations: [tailwind()],
+  output: "server",
+  adapter: netlify()
 });
