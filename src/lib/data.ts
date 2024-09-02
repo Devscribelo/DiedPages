fa71dceb15e9b682709bfd0a8ec02bfe9c43bcec
MBAPEINT <josecharey.2003@gmail.com>
2024-09-02 15:01:45 -0500

    remembersection-update

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -185,18 +185,18 @@ export interface Mensajes{
 
 export const mensajes: Mensajes[] = [
     {
-        img: 'background-image: url("https://i.ibb.co/8rBQVgp/003cdb51-bf60-4bc8-9e65-990e73eb2937.jpg");',
-        name: 'Jose Chacón',
+        img: 'background-image: url("https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg");',
+        name: 'Canicas',
         remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.'
     },
     {
-        img: 'background-image: url("https://i.ibb.co/8rBQVgp/003cdb51-bf60-4bc8-9e65-990e73eb2937.jpg");',
-        name: 'Jose Chacón',
+        img: 'background-image: url("https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg");',
+        name: 'Canicas',
         remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.'
     },
     {
-        img: 'background-image: url("https://i.ibb.co/8rBQVgp/003cdb51-bf60-4bc8-9e65-990e73eb2937.jpg");',
-        name: 'Jose Chacón',
+        img: 'background-image: url("https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg");',
+        name: 'Canicas',
         remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.'
     },
 ]
