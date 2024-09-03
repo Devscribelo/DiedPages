65a0f2941cc33adbca6fceb76cd5ab945d796d83
MBAPEINT <josecharey.2003@gmail.com>
2024-09-03 15:58:38 -0500

    data-update

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,36 +1,12 @@
 export const logo: string = '/logore.svg'
 export const name: string = 'Juan Gabriel León Carpio'
 
-export interface ImagesBiography {
+export interface Images {
     description: string;
     src: string;
 }
 
-export const bimages: ImagesBiography[] = [
-    {
-        description: '',
-        src: '/biography/1.jpeg'
-    },
-    {
-        description: '',
-        src: '/biography/2.jpeg'
-    },
-    {
-        description: '',
-        src: '/biography/3.jpeg'
-    },
-    {
-        description: '',
-        src: '/biography/4.jpeg'
-    }
-]
-
-export interface ImagesGallery {
-    description: string;
-    src: string;
-}
-
-export const gimages: ImagesGallery[] = [
+export const images: Images[] = [
     {
         description: '',
         src: '/gallery/1.jpg'
@@ -113,28 +89,12 @@ export const gimages: ImagesGallery[] = [
     }
 ]
 
-export interface BVideos{
+export interface Videos{
     href: string;
     img: string;
 }
 
-export const bvideos: BVideos[] = [
-    {
-        href:'https://www.youtube.com/watch?v=R0d56K_xOLM',
-        img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg'
-    },
-    {
-        href:'https://www.youtube.com/watch?v=R0d56K_xOLM',
-        img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg'
-    }
-]
-
-export interface GVideos{
-    href: string;
-    img: string;
-}
-
-export const gvideos: GVideos[] = [
+export const videos: Videos[] = [
     {
         href:'https://www.youtube.com/watch?v=R0d56K_xOLM',
         img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg'
@@ -196,6 +156,36 @@ export const mensajes: Mensajes[] = [
         name: 'Canicas',
         remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.',
         titulo: 'Recuerdo especial 2'
+    },
+    {
+        img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg',
+        name: 'Canicas',
+        remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.',
+        titulo: 'Recuerdo especial 3333'
+    },
+    {
+        img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg',
+        name: 'Canicas',
+        remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.',
+        titulo: 'Recuerdo especial 3333'
+    },
+    {
+        img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg',
+        name: 'Canicas',
+        remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.',
+        titulo: 'Recuerdo especial 3333'
+    },
+    {
+        img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg',
+        name: 'Canicas',
+        remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.',
+        titulo: 'Recuerdo especial 3333'
+    },
+    {
+        img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg',
+        name: 'Canicas',
+        remember: 'Con profunda tristeza y un corazón lleno de melancolía, quiero compartir un recuerdo especial de nuestro querido amigo Juan Gabriel. En cada rincón de nuestras memorias, su risa y su presencia aún resuenan con la misma calidez y alegría que siempre nos brindó. Aunque su ausencia deja un vacío profundo, su espíritu sigue vivo en cada historia y en cada sonrisa que compartimos en su honor.',
+        titulo: 'Recuerdo especial 3333'
     },
     {
         img: 'https://img.youtube.com/vi/R0d56K_xOLM/maxresdefault.jpg',
